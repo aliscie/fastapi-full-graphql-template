e87dc9b1acee68e519095581ba81febc2bf7ad88
AlenSci <ali.alkaraawi@crowdbotics.com>
2021-10-10 10:46:37 +0300

    0

--- OtherApp/main.py
+++ OtherApp/main.py
@@ -1,30 +1,57 @@
-import asyncio
+import json
+from collections import AsyncGenerator
+from typing import Any
 
-from ariadne import SubscriptionType
+from ariadne import SubscriptionType, MutationType
+from graphql import GraphQLResolveInfo
 
-from core.TypeDef import AddTypeDef
+from core.main import broadcast
+from db_conf import db_session
+
+db = db_session.session_factory()
 
 sub2 = SubscriptionType()
+send_mutation = MutationType()
 
 
-counter2_type_def = '''
-
-extend type Subscription {
-"""
-- This returns 0 value test, 1 value test, 3 value test
-"""
-counter2: String!
-}
-'''
-
-AddTypeDef(counter2_type_def)
-@sub2.source("counter2")
-async def counter_generator(obj, info):
-    for i in range(3):
-        await asyncio.sleep(0.3)
-        yield i
 
 
-@sub2.field("counter2")
+@send_mutation.field("send")
+async def resolve_send(*args, **kwargs):
+    number = kwargs.get('number')
+    await broadcast.publish(channel="chatroom", message=json.dumps({'message': number, "sender": 1}))
+
+    # for i in Post.query.all():
+    # sql1 = delete(Post).where(Post.id == i.id)
+    # db.execute(sql1)
+    # db.commit()
+
+    # db_post = models.Post(title=number)
+    # db.add(db_post)
+    # db.commit()
+    return number
+
+
+@sub2.source("chat")
+async def counter_generator(_: Any, info: GraphQLResolveInfo) -> AsyncGenerator[str, None]:
+    async with broadcast.subscribe(channel="chatroom") as subscriber:
+        async for event in subscriber:
+            yield json.loads(event.message)
+
+    #     yield str(subscriber)
+
+
+@sub2.field("chat")
 def counter_resolver(count, info):
-    return f'{count } value test'
+    return count
+
+
+# @event.listens_for(models.Post, 'after_insert')
+# def do_stuff(mapper, connection, target, *args, **kwargs):
+#     ic('do_stuffdo_stuffdo_stuffdo_stuff')
+#     # ic(mapper, connection, target, args, kwargs)
+
+
+types = [sub2, send_mutation]
+# AddTypeDef(chat_type_def)
+# AddType([sub2, send_mutation])
--- conftest.py
+++ conftest.py
@@ -1,15 +1,17 @@
+
+
 import pytest
+from ariadne.asgi import GraphQL
 from graphql import graphql_sync
-# from starlette.testclient import TestClient
+from starlette.testclient import TestClient
 
 from core.main import schema
 
 
-# @pytest.fixture
-# def client():
-#     app = GraphQL(schema)
-#     return TestClient(app)
-
+@pytest.fixture
+def client():
+    app = GraphQL(schema)
+    return TestClient(app)
 
 def parsequery(x):
     return graphql_sync(schema, x)
